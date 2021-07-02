ba22d780774fb8ad9622d7f3690eeee389feebc2
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-07-02 13:25:32 +0800

    CSS JS git

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title')</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="assets/vendors/iconfonts/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="assets/vendors/iconfonts/ionicons/dist/css/ionicons.css">
-    <link rel="stylesheet" href="assets/vendors/iconfonts/flag-icon-css/css/flag-icon.min.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.base.css">
-    <link rel="stylesheet" href="assets/vendors/css/vendor.bundle.addons.css">
+    <link rel="stylesheet" href="materialdesignicons.min.css">
+    <link rel="stylesheet" href="ionicons.css">
+    <link rel="stylesheet" href="flag-icon.min.css">
+    <link rel="stylesheet" href="vendor.bundle.base.css">
+    <link rel="stylesheet" href="vendor.bundle.addons.css">
     <!-- endinject -->
     <!-- plugin css for this page -->
     <!-- End plugin css for this page -->
@@ -18,9 +18,9 @@
     <link rel="stylesheet" href="assets/css/shared/style.css">
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="assets/css/demo_1/style.css">
+    <link rel="stylesheet" href="demo_1_style.css">
     <!-- End Layout styles -->
-    <link rel="shortcut icon" href="assets/images/favicon.ico" />
+    <link rel="shortcut icon" href="favicon.ico">
     @yield('css')
   </head>
   <body>
@@ -858,19 +858,19 @@
     </div>
     <!-- container-scroller -->
     <!-- plugins:js -->
-    <script src="assets/vendors/js/vendor.bundle.base.js"></script>
-    <script src="assets/vendors/js/vendor.bundle.addons.js"></script>
+    <script src="vendor.bundle.base.js"></script>
+    <script src="vendor.bundle.addons.js"></script>
     <!-- endinject -->
     <!-- Plugin js for this page-->
     <!-- End plugin js for this page-->
     <!-- inject:js -->
-    <script src="assets/js/shared/off-canvas.js"></script>
-    <script src="assets/js/shared/misc.js"></script>
+    <script src="off-canvas.js"></script>
+    <script src="misc.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="assets/js/demo_1/dashboard.js"></script>
+    <script src="dashboard.js"></script>
     <!-- End custom js for this page-->
-    <script src="assets/js/shared/jquery.cookie.js" type="text/javascript"></script>
+    <script src="jquery.cookie.js" type="text/javascript"></script>
     @yield('js')
   </body>
 </html>
