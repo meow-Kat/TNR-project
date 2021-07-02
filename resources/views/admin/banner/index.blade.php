cd79a5efb8752f6aff5a91a30ca36b338d4cb2f3
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2021-07-02 15:50:44 +0800

    update

---
+++ resources/views/admin/banner/index.blade.php
@@ -0,0 +1,58 @@
+@extends('template')
+
+@section('title', 'banner管理')
+
+
+
+@section('css')
+
+@endsection
+
+
+
+@section('content')
+<table id="myTable" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($collection as $item)
+            <tr>
+                <td>Tiger Nixon</td>
+                <td>System Architect</td>
+                <td>Edinburgh</td>
+                <td>61</td>
+                <td>2011/04/25</td>
+                <td>$320,800</td>
+            </tr>
+        @endforeach
+    </tbody>
+    <tfoot>
+        <tr>
+            <th>Name</th>
+            <th>Position</th>
+            <th>Office</th>
+            <th>Age</th>
+            <th>Start date</th>
+            <th>Salary</th>
+        </tr>
+    </tfoot>
+</table>
+@endsection
+
+
+
+@section('js')
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+        });
+    </script>
+@endsection
